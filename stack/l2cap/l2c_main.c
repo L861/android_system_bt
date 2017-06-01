84ab85616ab8e4949884907f68a2210a98525dc7
LineageOS-MTK <luzejij@binka.me>
2017-06-01 17:30:29 +0300

    Revert "resolve merge conflicts of a3ee2e35 to nyc-dev"
    This reverts commit a506b2fdd21041635f0649cdfcbdf83cea54db06.

--- stack/l2cap/l2c_main.c
+++ stack/l2cap/l2c_main.c
@@ -24,8 +24,6 @@
 
 #include <stdlib.h>
 #include <string.h>
-#include <log/log.h>
-
 #include <stdio.h>
 
 #include "device/include/controller.h"
@@ -191,13 +189,6 @@ void l2c_rcv_acl_data (BT_HDR *p_msg)
     STREAM_TO_UINT16 (hci_len, p);
     p_msg->offset += 4;
     L2CAP_TRACE_VERBOSE("%s: received packet from handle(%04x) of len (%d)", __FUNCTION__, handle, hci_len);
-    if (hci_len < L2CAP_PKT_OVERHEAD) {
-        /* Must receive at least the L2CAP length and CID */
-        L2CAP_TRACE_WARNING ("L2CAP - got incorrect hci header");
-        android_errorWriteLog(0x534e4554, "34946955");
-        osi_free(p_msg);
-        return;
-    }
 
     /* Extract the length and CID */
     STREAM_TO_UINT16 (l2cap_len, p);
@@ -221,8 +212,18 @@ void l2c_rcv_acl_data (BT_HDR *p_msg)
         }
     }
 
-    p_msg->len    = hci_len - L2CAP_PKT_OVERHEAD;
-    p_msg->offset += L2CAP_PKT_OVERHEAD;
+    if (hci_len >= L2CAP_PKT_OVERHEAD)  /* Must receive at least the L2CAP length and CID.*/
+    {
+        p_msg->len    = hci_len - L2CAP_PKT_OVERHEAD;
+        p_msg->offset += L2CAP_PKT_OVERHEAD;
+    }
+    else
+    {
+        L2CAP_TRACE_WARNING ("L2CAP - got incorrect hci header" );
+        GKI_freebuf (p_msg);
+        return;
+    }
+
     if (l2cap_len != p_msg->len)
     {
         L2CAP_TRACE_WARNING ("L2CAP - bad length in pkt. Exp: %d  Act: %d",
